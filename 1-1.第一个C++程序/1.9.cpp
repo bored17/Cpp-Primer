7eeaa19cb96ba943c7fc48ac02eb4524f49aecd0
=bored17 <=1029893460@qq.com>
2021-01-20 16:08:44 +0800

    add 1.9

---
+++ 1-1.第一个C++程序/1.9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int val = 50, sum = 0;
+	while (val<=100)
+	{
+		sum += val;
+		++val;
+	}
+	cout << "50 to 100 sum is" << sum << endl;
+	return 0;
+}
